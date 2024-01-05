60e4474c55c1ed71955f4fe028c0238806a19c82
Dmitry Korunov <dessel.k@gmail.com>
2024-01-05 23:56:53 +0400

    respect the build_embedded option at package compilation

--- lib/mix/tasks/compile_tbcd.ex
+++ lib/mix/tasks/compile_tbcd.ex
@@ -3,18 +3,29 @@ defmodule Mix.Tasks.Compile.Tbcd do
 
   @doc false
   def run(_args) do
-    {result, _errcode} = System.cmd(make_cmd(), ["-s", "-C", "c_src"], stderr_to_stdout: true)
+    priv? = File.dir?("priv")
+    Mix.Project.ensure_structure()
+    {result, _errcode} = System.cmd(make_cmd(), ["all", "-s", "-C", "c_src"], stderr_to_stdout: true)
     IO.binwrite(result)
-    :ok
+
+    # IF there was no priv before and now there is one, we assume
+    # the user wants to copy it. If priv already existed and was
+    # written to it, then it won't be copied if build_embedded is
+    # set to true.
+    if not priv? and File.dir?("priv") do
+      Mix.Project.build_structure()
+    end
+
+    {:ok, []}
   end
 
   # This is called by Elixir when `mix clean` runs
   # and `:tbcd` is in the list of compilers.
   @doc false
   def clean() do
-    {result, _errcode} = System.cmd(make_cmd(), ["clean","-C", "c_src"], stderr_to_stdout: true)
+    {result, _errcode} = System.cmd(make_cmd(), ["clean", "-C", "c_src"], stderr_to_stdout: true)
     IO.binwrite(result)
-    :ok
+    {:ok, []}
   end
 
   defp make_cmd() do
